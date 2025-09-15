6353201a84d4aaad4779032a6353a715cc779652
Filipe Lemos <contato@filipelemos.com>
2025-09-15 08:35:27 -0300

    Increasing buffer size

--- machines/nixos/default.nix
+++ machines/nixos/default.nix
@@ -50,6 +50,7 @@
   nix = {
     settings = {
       auto-optimise-store = true;
+      download-buffer-size = "524288000";
       experimental-features = ["nix-command" "flakes"];
       substituters = [
         "https://cache.nixos.org"
