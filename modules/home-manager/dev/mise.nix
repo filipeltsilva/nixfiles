c04f0e21669e730ade5d04c0aa4b51dc9a3194e0
Filipe Lemos <contato@filipelemos.com>
2026-03-18 12:57:03 -0300

    feat: add opencode

--- modules/home-manager/dev/mise.nix
+++ modules/home-manager/dev/mise.nix
@@ -13,28 +13,44 @@
       globalConfig = {
         tools = {
           node = "lts";
-          perl = "system";
-          python = "system";
-          ruby = "system";
+          perl = "latest";
+          # python = "latest";
+          # ruby = "latest";
           shellcheck = "latest";
         };
       };
     };
 
     # xdg.configFile."mise/default-npm-packages".text = ''
-    #   gitignore
     #   neovim
     #   npm
     # '';
+    #
+    # xdg.configFile."mise/default-python-packages".text = ''
+    #   pip
+    #   pynvim
+    # '';
+    #
+    # xdg.configFile."mise/default-ruby-gems".text = ''
+    #   bundler
+    #   neovim
+    # '';
 
-    # run mise install automatically after install mise
+    # post-install script
     home.activation.miseToolsInstall = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      export PATH="$HOME/.local/share/mise/shims:$PATH"
+
       echo "Installing mise tools"
       ${pkgs.mise}/bin/mise install
 
+      echo "Setting mise global tools versions"
+      ${pkgs.mise}/bin/mise use -g node@lts
+      ${pkgs.mise}/bin/mise use -g perl@latest
+      ${pkgs.mise}/bin/mise use -g python@latest
+      ${pkgs.mise}/bin/mise use -g shellcheck@latest
+
       echo "Installing global npm packages"
       ${pkgs.mise}/bin/mise exec node@lts -- npm install -g \
-        github \
         neovim \
         npm
     '';
