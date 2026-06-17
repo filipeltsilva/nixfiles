5d3d33d20d10b337d4cfb1660827e8ebb14a2b59
Filipe Lemos <contato@filipelemos.com>
2026-06-17 16:55:27 -0300

    feat: call homebrew during darwin-rebuild without asking for password

--- modules/darwin/core/user.nix
+++ modules/darwin/core/user.nix
@@ -4,5 +4,12 @@
       description = "${me.userFullName}";
       home = "/Users/${me.user}";
     };
+
+    # Allow run homebrew without requiring password during darwin-rebuild
+    security.sudo.extraConfig = ''
+      %admin ALL=(ALL) NOPASSWD: /run/current-system/sw/bin/darwin-rebuild
+      %admin ALL=(ALL) NOPASSWD: /run/current-system/sw/bin/nix*
+      %admin ALL=(ALL) NOPASSWD: /usr/local/bin/brew, /opt/homebrew/bin/brew
+    '';
   };
 }
